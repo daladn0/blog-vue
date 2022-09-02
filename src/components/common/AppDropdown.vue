aafc02dbaed2021053b563a8f9d6869ada325272
Mark Kostevych <markkostevych@gmail.com>
2022-09-02 23:09:08 +0300

    Dropdown

---
+++ src/components/common/AppDropdown.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="z-10 w-56 bg-white rounded shadow after:block after:absolute after:w-4 after:h-4"
+  >
+    <slot name="header" />
+    <ul class="text-sm text-gray-700 dark:text-gray-200">
+      <li
+        v-for="item in items"
+        :key="item.emit"
+        :class="{ 'border-t': item.separated }"
+        class="py-1"
+      >
+        <button
+          @click="$emit(item.emit)"
+          class="flex items-center w-full text-left py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white truncate"
+          :class="{
+            'text-center justify-center': item.textCenter,
+          }"
+        >
+          <svg
+            v-if="item.icon"
+            class="w-5 h-5 text-gray-500 mr-2 flex-shrink-0"
+          >
+            <use v-svg-icon="item.icon" />
+          </svg>
+          <span class="truncate">{{ item.title }}</span>
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: "AppDropdown",
+  props: {
+    items: {
+      type: Array,
+    },
+  },
+};
+</script>
--- src/components/layouts/AppHeader.vue
+++ src/components/layouts/AppHeader.vue
@@ -24,29 +24,104 @@
           Log in
         </router-link>
       </div>
-      <button
-        v-else
-        class="flex items-center transition-all hover:bg-slate-100 rounded-3xl px-3 py-1"
-      >
-        <span class="text-sm text-gray-700 mr-2 max-w-[150px] truncate block">{{
-          currentUser.fullName || "sadasd"
-        }}</span>
-        <img
-          class="w-8 h-8 rounded-full object-cover overflow-hidden"
-          :src="currentUser.avatar || 'asdas'"
-          :alt="currentUser.fullName || 'asdas'"
-        />
-      </button>
+      <div v-else class="relative">
+        <button
+          class="flex items-center transition-all hover:bg-slate-100 rounded-3xl px-3 py-1"
+          type="button"
+          id="profile"
+          @click="dropdownVisible = !dropdownVisible"
+          :class="{
+            'bg-slate-100': dropdownVisible,
+          }"
+        >
+          <span
+            class="text-sm text-gray-700 mr-2 max-w-[150px] truncate block"
+            >{{ currentUser.fullName }}</span
+          >
+          <img
+            class="w-8 h-8 rounded-full object-cover overflow-hidden"
+            :src="currentUser.avatar"
+            :alt="currentUser.fullName"
+          />
+        </button>
+
+        <transition name="slide-down">
+          <AppDropdown
+            v-if="dropdownVisible"
+            id="dropdown"
+            :items="dropList"
+            class="absolute right-0 top-full mt-6"
+          >
+            <template #header>
+              <div class="flex items-center max-w-full p-3">
+                <img
+                  class="flex-shrink-0 w-8 h-8 mr-2 rounded-full object-cover overflow-hidden"
+                  :src="currentUser.avatar"
+                  :alt="currentUser.fullName"
+                />
+                <div class="text-sm text-gray-900 dark:text-white truncate">
+                  <div class="font-medium truncate">
+                    {{ currentUser.fullName }}
+                  </div>
+                  <div class="truncate">{{ currentUser.email }}</div>
+                </div>
+              </div>
+            </template>
+          </AppDropdown>
+        </transition>
+      </div>
     </div>
   </nav>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import AppDropdown from "@/components/common/AppDropdown.vue";
 export default {
   name: "AppHeader",
   computed: {
     ...mapGetters("user", ["currentUser", "isAuth"]),
+  },
+  components: {
+    AppDropdown,
+  },
+  data() {
+    return {
+      dropdownVisible: false,
+      dropList: [
+        { title: "Dashboard", icon: "lock", emit: "dashboard" },
+        { title: "Settings", icon: "lock", emit: "settings" },
+        { title: "Earning", icon: "lock", emit: "earning" },
+        {
+          title: "Log out",
+          icon: "out",
+          emit: "logout",
+          separated: true,
+          textCenter: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    onWindowClick(e) {
+      const target = e.target;
+
+      if (
+        target.id === "profile" ||
+        target.id === "dropdown" ||
+        target.closest("#profile") ||
+        target.closest("#dropdown")
+      )
+        return;
+
+      this.dropdownVisible = false;
+    },
+  },
+  mounted() {
+    window.addEventListener("click", this.onWindowClick);
+  },
+  beforeUnmount() {
+    window.removeEventListener("click", this.onWindowClick);
   },
 };
 </script>
